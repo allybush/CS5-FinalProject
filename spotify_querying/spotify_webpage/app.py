d8b985bcfae73518282caaf643e6f4161055dd1e
Darrow8 <darrowrhartman@gmail.com>
2022-04-06 10:59:15 -0700

    made changes to get spotify code to work

--- spotify_querying/spotify_webpage/app.py
+++ spotify_querying/spotify_webpage/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__) #this has 2 underscores on each side
  
 
 response = []
-song_url = '';
+# song_url = '';
 
 
 @app.route('/',methods=['GET', 'POST']) 
@@ -18,15 +18,15 @@ def index():
 		search_query = request.form.get('search')
 		response = sp.search(search_query)
 		print(response)
-		return render_template('index.html.j2',results=response, song_url=song_url) 
+		return render_template('index.html.j2',results=response) 
 	return render_template('index.html.j2') 
 
 
 @app.route('/song',methods=['GET', 'POST'])
 def song():
 	print('here!')
-	song_url = request.args.get('url')
-	print(song_url)
+	# song_url = request.args.get('url')
+	# print(song_url)
 
 	# print('woah')
 	return render_template('index.html.j2', results=response, song_url=song_url) 	
